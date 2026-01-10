a6a4649c0f10cb16e96275f9be99acd1ac523b5c
Angelo Pirola <angelo9871@gmail.com>
2026-01-11 00:30:48 +0100

    Add PagedResult<T> class for paginated query results
    Introduced the PagedResult<T> class in the TinyRepository.Paging namespace to encapsulate paginated result sets. The class includes properties for items, total count, page number, and page size, along with both parameterless and parameterized constructors.

---
+++ src/TinyRepository/Paging/PagedResult.cs
@@ -0,0 +1,19 @@
+﻿namespace TinyRepository.Paging;
+
+public class PagedResult<T>
+{
+    public IEnumerable<T> Items { get; init; } = [];
+    public int TotalCount { get; init; }
+    public int PageNumber { get; init; }
+    public int PageSize { get; init; }
+
+    public PagedResult() { }
+
+    public PagedResult(IEnumerable<T> items, int totalCount, int pageNumber, int pageSize)
+    {
+        Items = items;
+        TotalCount = totalCount;
+        PageNumber = pageNumber;
+        PageSize = pageSize;
+    }
+}
