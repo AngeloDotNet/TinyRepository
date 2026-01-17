9c7205002d810d3ec56c53e043573c04d6c8ade9
Angelo Pirola <angelo9871@gmail.com>
2026-01-18 00:50:20 +0100

    Add SortDescriptor class for flexible sorting support
    Introduced the sealed SortDescriptor class in the TinyRepository.Sorting namespace. This class encapsulates a property name and sort direction (ascending or descending) for sorting operations, leveraging the SortDirection enum. Includes static helper methods for easy creation and enforces non-null property names.

---
+++ src/TinyRepository/Sorting/SortDescriptor.cs
@@ -0,0 +1,12 @@
+﻿using TinyRepository.Enums;
+
+namespace TinyRepository.Sorting;
+
+public sealed class SortDescriptor(string propertyName, SortDirection direction = SortDirection.Ascending)
+{
+    public string PropertyName { get; } = propertyName ?? throw new ArgumentNullException(nameof(propertyName));
+    public SortDirection Direction { get; } = direction;
+
+    public static SortDescriptor Asc(string propertyName) => new SortDescriptor(propertyName, SortDirection.Ascending);
+    public static SortDescriptor Desc(string propertyName) => new SortDescriptor(propertyName, SortDirection.Descending);
+}
