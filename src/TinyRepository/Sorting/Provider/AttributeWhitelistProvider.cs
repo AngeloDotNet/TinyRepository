6a612a84e6d7a786983afc95e65ce400e082d591
Angelo Pirola <angelo9871@gmail.com>
2026-01-23 00:22:48 +0100

    Add AttributeWhitelistProvider for orderable property DI
    Introduced AttributeWhitelistProvider<T> to build property whitelists using [Orderable] attributes. This provider implements IPropertyWhitelistProvider<T> and can be registered in DI to expose allowed sortable properties for a given type.

---
+++ src/TinyRepository/Sorting/Provider/AttributeWhitelistProvider.cs
@@ -0,0 +1,16 @@
+﻿using TinyRepository.Extensions;
+using TinyRepository.Provider;
+
+namespace TinyRepository.Sorting.Provider;
+
+/// <summary>
+/// Provider opzionale che costruisce la whitelist leggendo gli attributi [Orderable].
+/// Puoi registrarlo in DI se vuoi esporre esplicitamente una IPropertyWhitelistProvider<T>.
+/// </summary>
+public class AttributeWhitelistProvider<T> : IPropertyWhitelistProvider<T>
+{
+    public IEnumerable<string> GetAllowedProperties()
+    {
+        return OrderablePropertyScanner.GetOrderableProperties<T>();
+    }
+}
