7b0193976e3c08ba6f339721f976f3ce832ec43f
Angelo Pirola <angelo9871@gmail.com>
2026-01-27 00:41:29 +0100

    Add Author model with orderable FirstName and LastName
    Introduced the Author class in TinyRepository.Sample.Models.
    Both FirstName and LastName properties are nullable strings
    and marked with the [Orderable] attribute to support sorting.

---
+++ samples/TinyRepository.Sample/Models/Author.cs
@@ -0,0 +1,12 @@
+﻿using TinyRepository.Sorting;
+
+namespace TinyRepository.Sample.Models;
+
+public class Author
+{
+    [Orderable]                 // consentito: Author.LastName
+    public string? LastName { get; set; }
+
+    [Orderable]                 // consentito: Author.FirstName
+    public string? FirstName { get; set; }
+}
