c41240a2dfb31577f3a6464dec402122619c3bba
Angelo Pirola <angelo9871@gmail.com>
2026-01-29 00:28:09 +0100

    Add TestDbContext for test entity management
    Introduced TestDbContext in the TinyRepository.Tests.Data namespace, inheriting from DbContext. Added DbSet properties for Article, Author, Book, and Publisher entities. Overrode OnConfiguring to suppress in-memory transaction warnings. Included necessary using directives for EF Core and test models.

---
+++ tests/TinyRepository.Tests/Data/TestDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using TinyRepository.Tests.Models;
+
+namespace TinyRepository.Tests.Data;
+
+public class TestDbContext(DbContextOptions<TestDbContext> options) : DbContext(options)
+{
+    public DbSet<Article> Articles => Set<Article>();
+    public DbSet<Author> Authors => Set<Author>();
+    public DbSet<Book> Books => Set<Book>();
+    public DbSet<Publisher> Publishers => Set<Publisher>();
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        base.OnConfiguring(optionsBuilder);
+
+        optionsBuilder.ConfigureWarnings(warnings => warnings.Ignore(InMemoryEventId.TransactionIgnoredWarning));
+    }
+}
