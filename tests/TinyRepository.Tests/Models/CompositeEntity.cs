4974b1197df71afd5a93d053800397beb87e390f
Angelo Pirola <angelo9871@gmail.com>
2026-01-31 00:43:42 +0100

    Add CompositeEntity class for testing composite keys
    Introduced CompositeEntity in TinyRepository.Tests.Models, implementing IEntity<int> with Id, Key1, Key2, and Name properties to support testing scenarios involving composite keys.

---
+++ tests/TinyRepository.Tests/Models/CompositeEntity.cs
@@ -0,0 +1,11 @@
+﻿using TinyRepository.Entities;
+
+namespace TinyRepository.Tests.Models;
+
+public class CompositeEntity : IEntity<int>
+{
+    public int Id { get; set; }
+    public int Key1 { get; set; }
+    public int Key2 { get; set; }
+    public string? Name { get; set; }
+}
