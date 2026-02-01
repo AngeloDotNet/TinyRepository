ec6ee029a5dbbb284991666beb84591a16eff887
Angelo Pirola <angelo9871@gmail.com>
2026-02-01 09:16:41 +0100

    Add ISampleService interface and implementation
    Introduce ISampleService with methods for retrieving and creating SampleEntity instances. Implement SampleService using repository and unit of work patterns, supporting optional eager loading of related entities and proper initialization of CreatedAt and Author fields.

---
+++ samples/TinyRepository.Sample/Services/ISampleService.cs
@@ -0,0 +1,10 @@
+﻿using TinyRepository.Sample.Entities;
+
+namespace TinyRepository.Sample.Services;
+
+public interface ISampleService
+{
+    //Task<SampleEntity?> GetByIdAsync(int id, CancellationToken cancellationToken);
+    Task<SampleEntity?> GetByIdAsync(int id, string[]? include, CancellationToken cancellationToken);
+    Task<SampleEntity> CreateAsync(SampleEntity entity, CancellationToken cancellationToken);
+}
---
+++ samples/TinyRepository.Sample/Services/SampleService.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TinyRepository.Interfaces;
+using TinyRepository.Sample.Entities;
+
+namespace TinyRepository.Sample.Services;
+
+public class SampleService(IRepository<SampleEntity, int> repository, IUnitOfWork unitOfWork) : ISampleService
+{
+    //public async Task<SampleEntity?> GetByIdAsync(int id, CancellationToken cancellationToken)
+    //    => await repository.GetByIdAsync(id, cancellationToken);
+
+    public async Task<SampleEntity?> GetByIdAsync(int id, string[]? include, CancellationToken cancellationToken)
+    {
+        if (include is null || include.Length == 0)
+        {
+            return await repository.GetByIdAsync(id, cancellationToken);
+        }
+
+        var result = await repository.Query(true, include).FirstOrDefaultAsync(e => e.Id == id, cancellationToken);
+
+        return result;
+    }
+
+    public async Task<SampleEntity> CreateAsync(SampleEntity entity, CancellationToken cancellationToken)
+    {
+        var newEntity = new SampleEntity()
+        {
+            Name = entity.Name,
+            CreatedAt = DateTime.UtcNow,
+            Author = new Author
+            {
+                FirstName = entity.Author?.FirstName,
+                LastName = entity.Author?.LastName
+            }
+        };
+
+        await repository.AddAsync(newEntity, cancellationToken);
+        await unitOfWork.SaveChangesAsync(cancellationToken);
+
+        return newEntity;
+    }
+}
