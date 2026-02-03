973a9c617eb99dd090283d81d266d36f76d0dd2d
Angelo Pirola <angelo9871@gmail.com>
2026-02-03 01:33:09 +0100

    Add IAliasProvider<T> interface for alias-to-path mapping
    Introduced the IAliasProvider<T> interface in the TinyRepository.Sorting.Interfaces namespace. This interface provides a GetAliasMap() method to return a dictionary mapping string aliases to their corresponding property paths, supporting flexible property referencing in sorting scenarios.

---
+++ src/TinyRepository/Sorting/Interfaces/IAliasProvider.cs
@@ -0,0 +1,10 @@
+﻿namespace TinyRepository.Sorting.Interfaces;
+
+public interface IAliasProvider<T>
+{
+    /// <summary>
+    /// Ritorna una mappa alias -> actual path (es. "authorName" => "Author.LastName").
+    /// La mappa può essere vuota se non sono stati definiti alias.
+    /// </summary>
+    IDictionary<string, string> GetAliasMap();
+}
