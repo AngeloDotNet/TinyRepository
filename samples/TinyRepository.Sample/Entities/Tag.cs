4dd401435ee324f96ae759e35709551c31222dd2
Angelo Pirola <angelo9871@gmail.com>
2026-02-04 10:14:14 +0100

    Add Article and Tag entities with sorting/inclusion support
    Introduced Article and Tag entity classes. Article implements IEntity<int> and includes properties for Id, Title (orderable and includable), IsPublished, Author (includable), and Tags (includable). Tag includes Id and Name (includable). These entities support sorting and inclusion features for TinyRepository.

---
+++ samples/TinyRepository.Sample/Entities/Tag.cs
@@ -0,0 +1,11 @@
+﻿using TinyRepository.Sorting;
+
+namespace TinyRepository.Sample.Entities;
+
+public class Tag
+{
+    public int Id { get; set; }
+
+    [IncludeAllowed]
+    public string? Name { get; set; }
+}
