5938613c551f0b344ae93fd63936dccc95619f62
Angelo Pirola <angelo9871@gmail.com>
2026-02-05 00:35:04 +0100

    Add MetadataService DI extension and improve whitelist scan
    Introduce AddMetadataService for DI with configurable options and assembly scanning, reusing MaxDepth from AttributeWhitelistScanOptions if available. Register AttributeWhitelistScanOptions as a singleton. Update AddAttributeWhitelistScan to register IAliasProvider<> and improve code clarity.

--- src/TinyRepository/Extensions/ServiceCollectionExtensions.cs
+++ src/TinyRepository/Extensions/ServiceCollectionExtensions.cs
@@ -2,6 +2,8 @@ using System.Reflection;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using TinyRepository.Interfaces;
+using TinyRepository.Metadata;
+using TinyRepository.Metadata.Interfaces;
 using TinyRepository.Options;
 using TinyRepository.Provider;
 using TinyRepository.Provider.Interfaces;
@@ -21,13 +23,16 @@ public static class ServiceCollectionExtensions
         return services;
     }
 
-    public static IServiceCollection AddAttributeWhitelistScan(this IServiceCollection services, Action<AttributeWhitelistScanOptions>? configure = null,
-        params Assembly[]? assemblies)
+    public static IServiceCollection AddAttributeWhitelistScan(this IServiceCollection services,
+        Action<AttributeWhitelistScanOptions>? configure = null, params Assembly[]? assemblies)
     {
         var options = new AttributeWhitelistScanOptions();
         configure?.Invoke(options);
 
-        var scanAssemblies = (assemblies == null || assemblies.Length == 0) ? [Assembly.GetCallingAssembly()] : assemblies.Distinct().ToArray();
+        services.AddSingleton(options);
+
+        var scanAssemblies = (assemblies == null || assemblies.Length == 0)
+            ? [Assembly.GetCallingAssembly()] : assemblies.Distinct().ToArray();
 
         var types = scanAssemblies.SelectMany(a => a.GetTypes()).Where(t => t.IsClass && !t.IsAbstract).ToArray();
 
@@ -42,14 +47,34 @@ public static class ServiceCollectionExtensions
                 var providerType = typeof(AttributeWhitelistProvider<>).MakeGenericType(t);
                 var ifaceProp = typeof(IPropertyWhitelistProvider<>).MakeGenericType(t);
                 var ifaceInclude = typeof(IIncludeWhitelistProvider<>).MakeGenericType(t);
+                var ifaceAlias = typeof(IAliasProvider<>).MakeGenericType(t);
 
-                // create single instance per type, registered as both interfaces
                 var instance = Activator.CreateInstance(providerType, options.MaxDepth)!;
 
                 services.AddSingleton(ifaceProp, _ => instance);
                 services.AddSingleton(ifaceInclude, _ => instance);
+                services.AddSingleton(ifaceAlias, _ => instance);
             }
         }
+
+        return services;
+    }
+
+    public static IServiceCollection AddMetadataService(this IServiceCollection services, Action<MetadataServiceOptions>? configure = null, params Assembly[]? assemblies)
+    {
+        var opts = new MetadataServiceOptions();
+        configure?.Invoke(opts);
+
+        // If MaxDepth not set explicitly, try to reuse AttributeWhitelistScanOptions from DI (when AddAttributeWhitelistScan was called)
+        // We'll register MetadataService as a factory so it can read AttributeWhitelistScanOptions from service provider.
+        services.AddSingleton<IMetadataService>(sp =>
+        {
+            var maxDepth = opts.MaxDepth ?? sp.GetService<AttributeWhitelistScanOptions>()?.MaxDepth ?? 5;
+            var scanAssemblies = (assemblies == null || assemblies.Length == 0)
+                ? [Assembly.GetCallingAssembly()] : assemblies.Distinct().ToArray();
+
+            return new MetadataService(maxDepth, scanAssemblies);
+        });
 
         return services;
     }
