7d57d685778c93a2aaeaf028740cc0bb0cf941eb
Angelo Pirola <angelo9871@gmail.com>
2026-02-11 00:15:45 +0100

    Add AliasDbContext and IncludeDbContext for testing
    Introduce two new DbContext classes in TinyRepository.Tests.Data:
    - AliasDbContext with AliasEntity and primary key config
    - IncludeDbContext with Parent and Child entities
    These contexts support testing scenarios using models from TinyRepository.Tests.Models.

---
+++ tests/TinyRepository.Tests/Data/AliasDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TinyRepository.Tests.Models;
+
+namespace TinyRepository.Tests.Data;
+
+public class AliasDbContext : DbContext
+{
+    public AliasDbContext(DbContextOptions<AliasDbContext> options) : base(options) { }
+    public DbSet<AliasEntity> AliasEntities => Set<AliasEntity>();
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<AliasEntity>().HasKey(e => e.Id);
+    }
+}
---
+++ tests/TinyRepository.Tests/Data/IncludeDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TinyRepository.Tests.Models;
+
+namespace TinyRepository.Tests.Data;
+
+public class IncludeDbContext(DbContextOptions<IncludeDbContext> options) : DbContext(options)
+{
+    public DbSet<Parent> Parents => Set<Parent>();
+    public DbSet<Child> Children => Set<Child>();
+}
